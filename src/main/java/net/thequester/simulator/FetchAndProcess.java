be91c873c32aace50e8b1fac54b0486c38b80295
tdubravcevic <Tomislav.Dubravcevic@infobip.com>
2014-02-20 09:46:34 +0100

    initial commit

---
+++ src/main/java/net/thequester/simulator/FetchAndProcess.java
@@ -0,0 +1,17 @@
+package net.thequester.simulator;
+
+import net.thequester.model.Quest;
+
+/**
+ * @author tdubravcevic
+ */
+public class FetchAndProcess {
+
+
+    public void main(){
+
+        Quest quest = new Quest();
+
+    }
+
+}
