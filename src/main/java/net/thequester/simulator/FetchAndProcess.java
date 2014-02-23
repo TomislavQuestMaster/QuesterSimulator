1aefdfa5efdb531b3a8276bd6afc84f7b0ae1830
Tomislav <tomislav.dubravcevic@gmail.com>
2014-02-23 20:40:12 +0100

    removed questerLib dependency tested quester web support download servlet

--- src/main/java/net/thequester/simulator/FetchAndProcess.java
+++ src/main/java/net/thequester/simulator/FetchAndProcess.java
@@ -1,6 +1,11 @@
 package net.thequester.simulator;
 
-import net.thequester.model.Quest;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * @author tdubravcevic
@@ -8,10 +13,43 @@ import net.thequester.model.Quest;
 public class FetchAndProcess {
 
 
-    public void main(){
+    public static void main(String[] args) {
 
-        Quest quest = new Quest();
+        // Make sure that this directory exists
+        try {
 
+            System.out.println("Downloading");
+
+            saveFileFromUrlWithJavaIO("new.txt", "http://localhost:8080/download");
+
+            System.out.println("Downloaded");
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void saveFileFromUrlWithJavaIO(String fileName, String fileUrl) throws IOException {
+        BufferedInputStream in = null;
+        FileOutputStream fout = null;
+        try {
+            in = new BufferedInputStream(new URL(fileUrl).openStream());
+            fout = new FileOutputStream(fileName);
+
+            byte data[] = new byte[1024];
+            int count;
+            while ((count = in.read(data, 0, 1024)) != -1) {
+                fout.write(data, 0, count);
+            }
+        } finally {
+            if (in != null)
+                in.close();
+            if (fout != null)
+                fout.close();
+        }
     }
 
 }
