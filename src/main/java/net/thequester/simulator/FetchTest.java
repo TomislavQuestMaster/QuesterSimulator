180d5921fbfda23183f7e609eb639febcad214b4
tdubravcevic <Tomislav.Dubravcevic@infobip.com>
2014-02-24 12:21:45 +0100

    testtin download and fetch

--- src/main/java/net/thequester/simulator/FetchTest.java
+++ src/main/java/net/thequester/simulator/FetchTest.java
@@ -1,15 +1,21 @@
 package net.thequester.simulator;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.thequester.websupport.model.Filter;
+import net.thequester.websupport.model.QuestDetails;
 import net.thequester.websupport.serializator.JsonSerializer;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author tdubravcevic
@@ -18,27 +24,32 @@ public class FetchTest {
 
 	public static void main(String[] args) throws Exception {
 
-		Filter filter = new Filter(0.0,0.0,123.0);
+		Filter filter = new Filter(45.793364, 15.946323, 1113.32);
 
 		JsonSerializer serializer = new JsonSerializer();
 		String json = serializer.serialize(filter);
 
-		sendPost(json);
+		String quests = sendPost("Cvjetno");
+
+
+		//List<QuestDetails> details = (List<QuestDetails>) serializer.deserializeList(quests, QuestDetails.class);
+
+		//System.out.println(details.get(0).getQuestName());
+
+		//DownloadTest.saveFileFromUrlWithJavaIO("new.txt", "http://localhost:8080/download");
 	}
 
 
-	private static void sendPost(String json) throws Exception {
+	private static String sendPost(String json) throws Exception {
 
-		String url = "http://localhost:8080/fetch";
+		String url = "http://localhost:8080/download";
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-		//add reuqest header
 		con.setRequestMethod("POST");
 		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 
 
-		// Send post request
 		con.setDoOutput(true);
 		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
 		wr.writeBytes(json);
@@ -61,8 +72,11 @@ public class FetchTest {
 		in.close();
 
 		//print result
-		System.out.println(response.toString());
+		String quests = response.toString();
 
+		System.out.println(quests);
+
+		return quests;
 	}
 
 }
